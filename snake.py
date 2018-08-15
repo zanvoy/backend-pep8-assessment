ede609c414f03fea33be7752519b9bd6f853f354
Edwin Marshall <emarshall85@gmail.com>
2018-08-15 02:56:04 -0400

    added credit for original source

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 # SNAKES GAME
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+# Code courtesy of Sanchit Gangwar: https://gist.github.com/sanchitgangwar/2158089
 
 import curses
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
